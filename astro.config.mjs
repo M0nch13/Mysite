89073c81a7e97702661d387147b4523979db0b3a
M0nch13 <m0nchie.curliss@gmail.com>
2025-04-04 23:37:52 -0600

    Second Commit (Added Pages and Style Sections)

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://M0nch13.github.io',
-    base: 'my-repo',
+    base: 'my-site',
   })
