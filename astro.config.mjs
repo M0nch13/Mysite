07bb0eb66db428bcdca879775131ad7142e4bac3
M0nch13 <m0nchie.curliss@gmail.com>
2025-04-04 22:51:16 -0600

    Initial Commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://M0nch13.github.io',
+    base: 'my-repo',
+  })
