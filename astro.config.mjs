bc95717c90831f20527ee5fcfbe550ed194351b5
M0nch13 <m0nchie.curliss@gmail.com>
2025-04-23 11:03:08 -0600

    Project #5

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,3 +6,8 @@ export default defineConfig({
     site: 'https://M0nch13.github.io',
     base: 'my-site',
   })
+  import { defineConfig } from "astro/config";
+
+export default defineConfig({
+  site: "https://astrositemc.netlify.app/"
+});
